4b3b7fb9da1386195b4b4ff1250d75d7df28aaa5
John Heng <johnheng5@gmail.com>
2019-02-01 10:40:13 -0500

    Fade stuff in...?

--- portfolio/src/photo/index.js
+++ portfolio/src/photo/index.js
@@ -12,27 +12,51 @@ const PhotoSeries = props => {
     </section>
   );
 };
-const PhotoItem = props => {
-  return (
-    <div className="grid-item">
-      <div className="portfolio-content">
-        <figure>
-          <img src={props.photo.url} alt={props.photo.title} />
-        </figure>
-        <div className="entry-content flex flex-column align-items-center justify-content-center">
-          <h3>
-            <a href={"#" + props.photo.title}>{props.photo.title}</a>
-          </h3>
-          <ul className="flex flex-wrap justify-content-center">
-            {props.photo.tags.map((tag, i) => (
-              <li key={i}>
-                <a href={"#" + tag}>{tag}</a>
-              </li>
-            ))}
-          </ul>
+class PhotoItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { loaded: false };
+  }
+
+  render() {
+    return (
+      <div className="grid-item">
+        <div className="portfolio-content">
+          <figure>
+            {this.state.loaded ? null : (
+              <div
+                style={{
+                  backgroundColor: "#eee",
+                  height: "400px",
+                  width: "400px",
+                  transition: "all 0.35s"
+                }}
+              />
+            )}
+            <img
+              style={this.state.loaded ? { opacity: "1" } : { opacity: "0" }}
+              src={this.props.photo.url}
+              alt={this.props.photo.title}
+              onLoad={() => this.setState({ loaded: true })}
+            />
+          </figure>
+          <div className="entry-content flex flex-column align-items-center justify-content-center">
+            <h3>
+              <a href={"#" + this.props.photo.title}>
+                {this.props.photo.title}
+              </a>
+            </h3>
+            <ul className="flex flex-wrap justify-content-center">
+              {this.props.photo.tags.map((tag, i) => (
+                <li key={i}>
+                  <a href={"#" + tag}>{tag}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 export default PhotoSeries;
